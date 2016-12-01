0c30f65e1c264301f2776e3b07c04cf6a1ada791
kavanabhat <kavana.bhat@in.ibm.com>
2016-12-01 16:44:17 +0530

    oprofile stop script changes (#5)

--- oprofile/oprofile_stop.sh
+++ oprofile/oprofile_stop.sh
@@ -4,7 +4,7 @@ ${WORKDIR?"Need to set WORKDIR env"} 2>/dev/null
 ${HADOOP_HOME?"Need to set HADOOP_HOME env"} 2>/dev/null
 
 echo "Stopping oprofile on all nodes of the cluster"
-AN "ps -ef | grep -w  operf | grep -v grep | awk '{print \$2}' | xargs -i sudo kill -SIGINT {}"
+AN "ps -ef | grep -w  'sudo.*operf' | grep -v grep | awk '{print \$2}' | xargs -i sudo kill -SIGINT {}"
 
 sleep 30
 
@@ -12,12 +12,12 @@ cd $WORKDIR/oprofile
 
 mkdir -p $WORKDIR/oprofile/all_reps/report.$$
 
-mv ./report/* ./all_reps/report.$$/
-
 for slaves in `cat $HADOOP_HOME/etc/hadoop/slaves | grep -v "^#" `
 do
 	scp ${slaves}:$HOME/oprofile/report/* $WORKDIR/oprofile/all_reps/report.$$/
 done
+
+mv ./report/* ./all_reps/report.$$/
 
 cd all_reps
 
